b6fcd38f485b677b9f67bd2a148c32d4bac608eb
zagorski32611 <j.j.zagorski@gmail.com>
2017-09-05 20:46:00 -0400

    render html not text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Hello, this is the sample app!"
+    render html: "Hello, this is the sample app!"
   end
 end
