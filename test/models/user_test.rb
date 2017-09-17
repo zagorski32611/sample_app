7b595beff1ff7e8ea2ab50865ded12797afb8d1c
zagorski32611 <j.j.zagorski@gmail.com>
2017-09-17 19:55:33 -0400

    removed test 'user should be valid'

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,10 +5,6 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: 'Example User', email: 'user@example.com')
   end
 
-  test 'should be valid' do
-    assert @user.valid?
-  end
-
   test 'name should be present' do
     @user.name = ''
     assert_not @user.valid?
