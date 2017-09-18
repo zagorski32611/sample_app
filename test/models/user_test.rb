58bd9e760e8b395d18cd15e0e9a35d7f26f4d96b
zagorski32611 <j.j.zagorski@gmail.com>
2017-09-17 20:06:25 -0400

    added password validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -34,4 +34,16 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
+
+  test 'password should be present' do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test 'password minimum length' do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+
 end
