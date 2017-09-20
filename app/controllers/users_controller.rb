a04509f83a6e813bfb7049b4c78611d8ace96e98
Joe Zagorski <j.j.zagorski@gmail.com>
2017-09-19 21:49:47 -0400

    added users params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
   end
@@ -17,5 +16,10 @@ class UsersController < ApplicationController
     end
   end
 
+  private
 
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
