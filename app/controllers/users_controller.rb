76dee126a113d3658b5fb85489982e584724a504
Joe Zagorski <j.j.zagorski@gmail.com>
2017-09-19 23:08:03 -0400

    more stuff on controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       # Handle a successful signup
     else
