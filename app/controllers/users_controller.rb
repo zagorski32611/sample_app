8014c0ac04d22c9a97103b52cf2d24614c0c2bac
zagorski32611 <j.j.zagorski@gmail.com>
2017-09-20 20:45:00 -0400

    added flash to applicaiton.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Successfully created your profile!'
+      flash[:success] = 'Welcome to my sample app!'
       redirect_to @user
     else
       render 'new'
