12c092e3fa1480b83cb92d423f259c30843b7f05
zagorski32611 <j.j.zagorski@gmail.com>
2017-09-24 14:58:11 -0400

    added jquery & dropdown menu

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'jquery-rails'
+
 gem 'bootstrap-sass'
 
 group :development, :test do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//= require jquery
 //= require rails-ujs
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
