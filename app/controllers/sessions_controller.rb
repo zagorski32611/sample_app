490fc39af0ec4b0960be55422a57d3e723c2a0b2
zagorski32611 <j.j.zagorski@gmail.com>
2017-09-24 10:31:41 -0400

    added sessions controller & test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log the user in
+    else
+      flash.now[:danger] = 'Invalid email or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
