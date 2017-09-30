f7e2d64c1b2b3650a2427803f371af4fea79a9f5
zagorski32611 <j.j.zagorski@gmail.com>
2017-09-30 12:53:36 -0400

    added users index, link, css & test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
@@ -37,6 +37,10 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   private
