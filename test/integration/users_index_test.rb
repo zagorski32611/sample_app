61e0e649cbd94a2091a2701796da9d55c2928c37
zagorski32611 <j.j.zagorski@gmail.com>
2017-10-01 15:05:45 -0400

    added paginate test, fails

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'index including paginate' do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.paginate'
+    User.paginate(page: 1).each do |user|
+      assert_select 'a[href=?]', user_path(user), test: user.name
+    end
+   end
+  end
