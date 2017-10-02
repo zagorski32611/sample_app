abe6dc5840805f97005263575687678caa4aa1ea
Joe Zagorski <j.j.zagorski@gmail.com>
2017-10-01 21:32:13 -0400

    added email conf controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,9 @@
 User.create!(name: 'Example User', email: 'examples@railstutorial.org',
              password: 'foobar', password_confirmation: 'foobar', admin: true)
 
+User.create!(name: 'Joe', email: 'joe@example.com', password: 'password',
+             password_confirmation: 'password', admin: true)
+
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
